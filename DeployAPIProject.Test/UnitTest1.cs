427116e5b18a457196ceacd5fb5a5462718295d8
learndotnet78 <learndotnet78@hotmail.com>
2024-03-19 15:13:56 -0400

    Initial commit

---
+++ DeployAPIProject.Test/UnitTest1.cs
@@ -0,0 +1,21 @@
+namespace DeployAPIProject.Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+
+        public void Test2()
+        {
+            Assert.Pass();
+        }
+    }
+}
